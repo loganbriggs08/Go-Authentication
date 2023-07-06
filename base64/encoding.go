4d397613a28aa474a27955885cb8bbe940b511c9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-06 19:40:38 +0100

    Encode passed through string and return the string in base64 format

--- base64/encoding.go
+++ base64/encoding.go
@@ -4,7 +4,8 @@ import (
 	"encoding/base64"
 )
 
-func Encode(text_to_encode any) {
+func Encode(text_to_encode string) string {
+	base_64_encode_result := base64.RawStdEncoding.EncodeToString([]byte(text_to_encode))
 
-	base64.RawStdEncoding.EncodeToString([]byte(message_to_encode))
+	return base_64_encode_result
 }
