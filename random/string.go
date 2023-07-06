52b80fe2d3c1d3c234531342b8915bc6a0f7111e
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-06 19:55:05 +0100

    Create functioning random string function

--- random/string.go
+++ random/string.go
@@ -1,5 +1,20 @@
 package random
 
-func String() {
+import (
+	"math/rand"
+	"time"
+)
 
+func String(length int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	end_string := make([]rune, length)
+
+	for i := range end_string {
+		end_string[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(end_string)
 }
