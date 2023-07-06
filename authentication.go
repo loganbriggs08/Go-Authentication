1342b9f1874ef404c5435e6336869743847cda7a
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-06 19:47:29 +0100

    Create get_authentication_token function

--- authentication.go
+++ authentication.go
@@ -6,6 +6,10 @@ import (
 	"github.com/NotKatsu/Go-Authentication/base64"
 )
 
+func get_authentication_token(unique_id int) string {
+
+}
+
 func main() {
 	encode_result := base64.Encode("this is my text")
 	decode_result := base64.Decode(encode_result)
