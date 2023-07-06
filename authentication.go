ac13151c1aaaaf3297b4f7976b1edf3329bf99a1
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-06 19:46:24 +0100

    Add some tests to test the encoding and decoding

--- authentication.go
+++ authentication.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/NotKatsu/Go-Authentication/base64"
+)
+
+func main() {
+	encode_result := base64.Encode("this is my text")
+	decode_result := base64.Decode(encode_result)
+
+	fmt.Println(encode_result, decode_result)
 }
