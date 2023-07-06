521139f9f0688c54f56554057289deb5ff2a3402
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-06 19:43:11 +0100

    Make function functionable to decode text

--- base64/decoding.go
+++ base64/decoding.go
@@ -1,5 +1,15 @@
 package helpers
 
-func Decode(text_to_decode string) string {
+import (
+	"encoding/base64"
+)
 
+func Decode(text_to_decode string) string {
+	base64_decode_result, err := base64.RawStdEncoding.DecodeString(text_to_decode)
+
+	if err == nil {
+		return string(base64_decode_result)
+	} else {
+		return ""
+	}
 }
